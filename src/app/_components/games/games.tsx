90d6430c9f11394ecedba77350ca8b52f06e1d8c
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2025-05-16 15:45:54 +0300

    feat(home): 2048 card

--- src/app/_components/games/games.tsx
+++ src/app/_components/games/games.tsx
@@ -3,12 +3,13 @@ import { ROUTING } from '@/app/routing';
 import GameCard from './game.card';
 
 import fifteenPuzzle from '@/assets/games/15-puzzle.svg';
+import game2048 from '@/assets/games/2048.svg';
 import ticTacToe from '@/assets/games/tic-tac-toe.svg';
 
 const GAME_LIST = [
   { title: 'Tic-Tac-Toe', href: ROUTING.TIC_TAC_TOE, src: ticTacToe },
   { title: '15 Puzzle', href: ROUTING.FIFTEEN_PUZZLE, src: fifteenPuzzle },
-  { title: '2048', href: ROUTING.GAME_2048, src: fifteenPuzzle },
+  { title: '2048', href: ROUTING.GAME_2048, src: game2048 },
 ];
 
 export default function Games() {
