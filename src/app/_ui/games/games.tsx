42ba61de1813343722de49a6e6556454374ef69e
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2025-05-21 19:43:20 +0300

    feat(home): sudoku card

--- src/app/_ui/games/games.tsx
+++ src/app/_ui/games/games.tsx
@@ -4,13 +4,14 @@ import { GameCard } from './game.card';
 
 import fifteenPuzzle from '@/assets/games/15-puzzle.svg';
 import game2048 from '@/assets/games/2048.svg';
+import sudoku from '@/assets/games/sudoku.svg';
 import ticTacToe from '@/assets/games/tic-tac-toe.svg';
 
 const GAME_LIST = [
   { title: 'Tic-Tac-Toe', href: ROUTING.TIC_TAC_TOE, src: ticTacToe },
   { title: '15 Puzzle', href: ROUTING.FIFTEEN_PUZZLE, src: fifteenPuzzle },
   { title: '2048', href: ROUTING.GAME_2048, src: game2048 },
-  { title: 'Sudoku', href: ROUTING.SUDOKU, src: game2048 },
+  { title: 'Sudoku', href: ROUTING.SUDOKU, src: sudoku },
 ];
 
 export function Games() {
